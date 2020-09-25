10a0fa78ce6620ae48edd0d682113be191d0e454
steveraxo <steve@raxo.tv>
2020-09-25 07:37:00 -0400

    fixed frontend redirect

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import AOS from "aos"
 import "aos/dist/aos.css"
