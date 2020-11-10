575e4138c4b670081377611a185d5f4b1a3c17db
Wistremiro Pulido <wistremiro@raxo.tv>
2020-11-09 20:46:18 -0400

    fixing mailchimp form

--- src/components/addCulture/newsletter/index.js
+++ src/components/addCulture/newsletter/index.js
@@ -83,7 +83,11 @@ const Newsletter = props => {
         // )
         // xhr.send()
 
-        addToMailchimp(email, fields)
+        addToMailchimp(
+          email,
+          fields,
+          "https://raxo.us18.list-manage.com/subscribe/post?u=0a73a5fcd4be545fba0c4bac9&amp;id=d80e65e5f2&SIGNUP=Blogsite"
+        )
           .then(data => {
             console.log(data)
             setStatus(data.result)
