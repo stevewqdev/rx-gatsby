3b94985eef2ae6232f498a907bcb3ea8e7262b4d
Wistremiro Pulido <wistremiro@raxo.tv>
2021-06-15 17:24:08 -0400

    fix load issue

--- src/pages/project/julie-and-the-phantoms.js
+++ src/pages/project/julie-and-the-phantoms.js
@@ -4,8 +4,8 @@ import axios from "axios"
 import { Helmet } from "react-helmet"
 import ProjectHero from "../../components/projects/header/index"
 import "../../layouts/pages/projects/jatp.css"
-// import Vimeo from "@u-wave/react-vimeo"
-// import Loading from "../../components/loading"
+import Vimeo from "@u-wave/react-vimeo"
+import Loading from "../../components/loading"
 import RelatedProjects from "../../components/relatedProjects/index"
 
 export default class JATP extends Component {
@@ -16,6 +16,7 @@ export default class JATP extends Component {
       videoIndex: 0,
       volume: 1,
       paused: false,
+      data: {},
     }
 
     this.handlePause = this.handlePause.bind(this)
@@ -56,7 +57,8 @@ export default class JATP extends Component {
       .then(({ data }) => {
         this.setState(
           {
-            data,
+            data: data,
+            isLoaded: true,
           },
           () => {
             console.log(this.state.data)
@@ -82,193 +84,191 @@ export default class JATP extends Component {
           <meta charSet="utf-8" />
           <title>Julie And The Phantoms: A Netflix Original - Raxo </title>
         </Helmet>
-        <div className="project__template jatp">
-          <ProjectHero
-            title={data.acf.frontend_title}
-            clientName={data.acf.client}
-            projectInfo={data.content.rendered}
-            projectFocus={data.acf.project_focus}
-            projectThumbnail={data.acf.featured_image}
-            theme={`${data.acf.project_background} jatp`}
-          ></ProjectHero>
+        {this.state.isLoaded === true ? (
+          <div className="project__template jatp">
+            <ProjectHero
+              title={data.acf.frontend_title}
+              clientName={data.acf.client}
+              projectInfo={data.content.rendered}
+              projectFocus={data.acf.project_focus}
+              projectThumbnail={data.acf.featured_image}
+              theme={`${data.acf.project_background} jatp`}
+            ></ProjectHero>
 
-          <div className="main__section__wrapper">
-            <section id="section__one">
-              <div className="container container__custom">
-                <div className="row one">
-                  <div className="col-lg-12 copy no__padding">
-                    <div
-                      className="simple__copy sm__font reg__font"
-                      dangerouslySetInnerHTML={{
-                        __html: data.acf.jatp_sone_copy,
-                      }}
-                    />
-                  </div>
-                  <div className="col-lg-12 image no__padding">
-                    <img
-                      src={data.acf.jatp_sone_image}
-                      alt={`Illustration from jatp project`}
-                      tabIndex={-1}
-                      loading="lazy"
-                    />
-                  </div>
-                </div>
-              </div>
-            </section>
-            <section className="includes__separator jatp" id="section__two">
-              <div className="container container__custom">
-                <div className="row section__two__about">
-                  <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 full__width__image ">
-                    <div className="separator --black"></div>
-                    <div
-                      data-aos="fade-up"
-                      data-aos-easing="ease-in-back"
-                      data-aos-delay={`120`}
-                      data-aos-duration="1200"
-                      data-aos-offset="300"
-                    >
-                      <h1
-                        className="xxl__font"
+            <div className="main__section__wrapper">
+              <section id="section__one">
+                <div className="container container__custom">
+                  <div className="row one">
+                    <div className="col-lg-12 copy no__padding">
+                      <div
+                        className="simple__copy sm__font reg__font"
                         dangerouslySetInnerHTML={{
-                          __html: data.acf.jatp_sone_about,
+                          __html: data.acf.jatp_sone_copy,
                         }}
+                      />
+                    </div>
+                    <div className="col-lg-12 image no__padding">
+                      <img
+                        src={data.acf.jatp_sone_image}
+                        alt={`Illustration from jatp project`}
+                        tabIndex={-1}
                       />
                     </div>
                   </div>
                 </div>
-              </div>
-            </section>
-
-            <section id="section__three">
-              <div className="container container__custom">
-                <div className="row">
-                  {data.acf.jatp_stwo_grid.map((element, index) => (
-                    <>
-                      {index !== 0 && index % 2 === 0 ? (
-                        <>
-                          <div
-                            className={`col-xs-12 col-sm-12 col-md-12 col-lg-3 copy hidden-${index}`}
-                          >
-                            <div
-                              className="simple__copy sm__font reg__font"
-                              dangerouslySetInnerHTML={{
-                                __html: element.copy,
-                              }}
-                            />
-                          </div>
-                          <div
-                            className={`col-xs-12 col-sm-12 col-md-12 col-lg-9 image hidden-${index} no__padding`}
-                          >
-                            <img
-                              src={element.main_image}
-                              alt={`Illustration from jatp project`}
-                              tabIndex={-1}
-                              loading="lazy"
-                            />
-                          </div>
-
-                          <div
-                            className={`col-xs-12 col-sm-12 col-md-12 col-lg-12 video no__padding hidden-${index}`}
-                          >
-                            {element.vimeo_id ? (
-                              <a href={element.vimeo_id} target="_BLANK">
-                                <div
-                                  className={`col-xs-12 col-sm-12 col-md-12 col-lg-12 image hidden-${index} no__padding`}
-                                >
-                                  <img
-                                    src={element.video_image}
-                                    alt={`Illustration from jatp project`}
-                                    tabIndex={-1}
-                                    loading="lazy"
-                                  />
-                                </div>
-                              </a>
-                            ) : (
-                              ""
-                            )}
-                          </div>
-                          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 end__image no__padding">
-                            {element.end_image ? (
-                              <img
-                                src={element.end_image}
-                                alt={`Illustration from jatp project`}
-                                tabIndex={-1}
-                                loading="lazy"
-                              />
-                            ) : (
-                              ""
-                            )}
-                          </div>
-                        </>
-                      ) : (
-                        <>
-                          <div
-                            className={`col-xs-12 col-sm-12 col-md-12 col-lg-9 image hidden-${index} no__padding`}
-                          >
-                            <img
-                              src={element.main_image}
-                              alt={`Illustration from jatp project`}
-                              tabIndex={-1}
-                              loading="lazy"
-                            />
-                          </div>
-                          <div
-                            className={`col-xs-12 col-sm-12 col-md-12 col-lg-3 copy hidden-${index}`}
-                          >
-                            <div
-                              className="simple__copy sm__font reg__font"
-                              dangerouslySetInnerHTML={{
-                                __html: element.copy,
-                              }}
-                            />
-                          </div>
-                          <div
-                            className={`col-xs-12 col-sm-12 col-md-12 col-lg-12 video no__padding hidden-${index}`}
-                          >
-                            {element.vimeo_id ? (
-                              <a href={element.vimeo_id} target="_BLANK">
-                                <div
-                                  className={`col-xs-12 col-sm-12 col-md-12 col-lg-12 image hidden-${index} no__padding`}
-                                >
-                                  <img
-                                    src={element.video_image}
-                                    alt={`Illustration from jatp project`}
-                                    tabIndex={-1}
-                                    loading="lazy"
-                                  />
-                                </div>
-                              </a>
-                            ) : (
-                              ""
-                            )}
-                          </div>
-                          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 end__image no__padding">
-                            {element.end_image ? (
-                              <img
-                                src={element.end_image}
-                                alt={`Illustration from jatp project`}
-                                tabIndex={-1}
-                              />
-                            ) : (
-                              ""
-                            )}
-                          </div>
-                        </>
-                      )}
-                    </>
-                  ))}
+              </section>
+              <section className="includes__separator jatp" id="section__two">
+                <div className="container container__custom">
+                  <div className="row section__two__about">
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 full__width__image ">
+                      <div className="separator --black"></div>
+                      <div
+                        data-aos="fade-up"
+                        data-aos-easing="ease-in-back"
+                        data-aos-delay={`120`}
+                        data-aos-duration="1200"
+                        data-aos-offset="300"
+                      >
+                        <h1
+                          className="xxl__font"
+                          dangerouslySetInnerHTML={{
+                            __html: data.acf.jatp_sone_about,
+                          }}
+                        />
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </section>
+              </section>
 
-            <RelatedProjects
-              relatedcopy={data.acf.related_extra_copy}
-              relatedproject={data.acf.related_project}
-              relatedthumbnail={data.acf.related_project_thumbnail}
-              relatedprojectlink={data.acf.related_project_url}
-            ></RelatedProjects>
+              <section id="section__three">
+                <div className="container container__custom">
+                  <div className="row">
+                    {data.acf.jatp_stwo_grid.map((element, index) => (
+                      <>
+                        {index !== 0 && index % 2 === 0 ? (
+                          <>
+                            <div
+                              className={`col-xs-12 col-sm-12 col-md-12 col-lg-3 copy hidden-${index}`}
+                            >
+                              <div
+                                className="simple__copy sm__font reg__font"
+                                dangerouslySetInnerHTML={{
+                                  __html: element.copy,
+                                }}
+                              />
+                            </div>
+                            <div
+                              className={`col-xs-12 col-sm-12 col-md-12 col-lg-9 image hidden-${index} no__padding`}
+                            >
+                              <img
+                                src={element.main_image}
+                                alt={`Illustration from jatp project`}
+                                tabIndex={-1}
+                              />
+                            </div>
+
+                            <div
+                              className={`col-xs-12 col-sm-12 col-md-12 col-lg-12 video no__padding hidden-${index}`}
+                            >
+                              {element.vimeo_id ? (
+                                <a href={element.vimeo_id} target="_BLANK">
+                                  <div
+                                    className={`col-xs-12 col-sm-12 col-md-12 col-lg-12 image hidden-${index} no__padding`}
+                                  >
+                                    <img
+                                      src={element.video_image}
+                                      alt={`Illustration from jatp project`}
+                                      tabIndex={-1}
+                                    />
+                                  </div>
+                                </a>
+                              ) : (
+                                ""
+                              )}
+                            </div>
+                            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 end__image no__padding">
+                              {element.end_image ? (
+                                <img
+                                  src={element.end_image}
+                                  alt={`Illustration from jatp project`}
+                                  tabIndex={-1}
+                                />
+                              ) : (
+                                ""
+                              )}
+                            </div>
+                          </>
+                        ) : (
+                          <>
+                            <div
+                              className={`col-xs-12 col-sm-12 col-md-12 col-lg-9 image hidden-${index} no__padding`}
+                            >
+                              <img
+                                src={element.main_image}
+                                alt={`Illustration from jatp project`}
+                                tabIndex={-1}
+                              />
+                            </div>
+                            <div
+                              className={`col-xs-12 col-sm-12 col-md-12 col-lg-3 copy hidden-${index}`}
+                            >
+                              <div
+                                className="simple__copy sm__font reg__font"
+                                dangerouslySetInnerHTML={{
+                                  __html: element.copy,
+                                }}
+                              />
+                            </div>
+                            <div
+                              className={`col-xs-12 col-sm-12 col-md-12 col-lg-12 video no__padding hidden-${index}`}
+                            >
+                              {element.vimeo_id ? (
+                                <a href={element.vimeo_id} target="_BLANK">
+                                  <div
+                                    className={`col-xs-12 col-sm-12 col-md-12 col-lg-12 image hidden-${index} no__padding`}
+                                  >
+                                    <img
+                                      src={element.video_image}
+                                      alt={`Illustration from jatp project`}
+                                      tabIndex={-1}
+                                    />
+                                  </div>
+                                </a>
+                              ) : (
+                                ""
+                              )}
+                            </div>
+                            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 end__image no__padding">
+                              {element.end_image ? (
+                                <img
+                                  src={element.end_image}
+                                  alt={`Illustration from jatp project`}
+                                  tabIndex={-1}
+                                />
+                              ) : (
+                                ""
+                              )}
+                            </div>
+                          </>
+                        )}
+                      </>
+                    ))}
+                  </div>
+                </div>
+              </section>
+
+              <RelatedProjects
+                relatedcopy={data.acf.related_extra_copy}
+                relatedproject={data.acf.related_project}
+                relatedthumbnail={data.acf.related_project_thumbnail}
+                relatedprojectlink={data.acf.related_project_url}
+              ></RelatedProjects>
+            </div>
           </div>
-        </div>
+        ) : (
+          <Loading />
+        )}
       </Layout>
     )
   }
